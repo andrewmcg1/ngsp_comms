5db4e6515aaead4a6035ab14aab2fea3e6c38e51
Andrew <greekgeek191@gmail.com>
2025-02-25 20:03:22 -0800

    updated with plots of BER and Eb/No

--- BudgetData.py
+++ BudgetData.py
@@ -5,6 +5,7 @@ from agi.stk12.stkutil import *
 from agi.stk12.vgt import *
 
 import json
+import matplotlib.pyplot as plt
 
 network_json = "atlasNetwork/atlas.json"
 
@@ -22,8 +23,11 @@ root.UnitPreferences.SetCurrentUnit("Time", "sec")
 root.UnitPreferences.SetCurrentUnit("Angle", "deg")
 root.UnitPreferences.SetCurrentUnit("Latitude", "deg")
 root.UnitPreferences.SetCurrentUnit("Longitude", "deg")
-root.UnitPreferences.SetCurrentUnit("DateFormat", "EpSec")
+root.UnitPreferences.SetCurrentUnit("DateFormat", "UTCG")
 root.UnitPreferences.SetCurrentUnit("Duration", "sec")
+
+root.CurrentScenario.SetTimePeriod("21 Feb 2025 00:00:00", "22 Feb 2025 00:00:00")
+root.UnitPreferences.SetCurrentUnit("DateFormat", "EpSec")
 
 
 print("Preparing Satellite...")
@@ -66,7 +70,7 @@ sat_tx_model = sat_tx.Model
 sat_tx_model.Frequency = sat_tx_freq/1000
 sat_tx_model.DataRate = downlinkDataRate
 sat_tx_model.Power = 1 # dBW
-sat_tx_model.AntennaGain = 20 # dB
+sat_tx_model.AntennaGain = 15 # dB
 
 sat_rx = refueler_sat.Children.New(AgESTKObjectType.eReceiver, 'SatReceiver')
 sat_rx.SetModel('Simple Receiver Model')
@@ -79,6 +83,7 @@ with open(network_json) as f:
     data = json.load(f)
 
 ground_stations = []
+downlink = []
 
 print("Preparing Ground Stations...")
 for i, station in enumerate(data['ground-stations']['enterprise']):
@@ -119,13 +124,42 @@ for i, station in enumerate(data['ground-stations']['enterprise']):
     gnd_rx_model = gnd_rx.Model
     gnd_rx_model.GOverT = station['frequency']['s_band']['G/T_db_K']
 
-    linkAccess = refueler_sat.GetAccessToObject(ground_stations[i])
-    linkAccess.ComputeAccess()
+    downlinkAccess = sat_tx.GetAccessToObject(gnd_rx)
+    downlinkAccess.ComputeAccess()
 
-    accessIntervals = linkAccess.ComputedAccessIntervalTimes
-    #print("\t\tAccess Intervals: ", accessIntervals.Time)
+    accessIntervals = downlinkAccess.ComputedAccessIntervalTimes.ToArray(0, -1)
+    data_provider = downlinkAccess.DataProviders.Item("Link Information")
 
+    link_budget_values = [["Time"], ["Eb/No"], ["BER"]]
+    time_values = []
+    ebno = []
+    ber = []
 
+    for interval in accessIntervals:
+        data = data_provider.ExecElements(interval[0], interval[1], 60, link_budget_values)
+        time_values.append(data.DataSets[0].GetValues())
+        ebno.append(data.DataSets[1].GetValues())
+        ber.append(data.DataSets[2].GetValues())
+    
+    downlink.append([time_values, ebno, ber])
+
+    for i in range(len(accessIntervals)):
+        plt.figure(1)
+        plt.plot(time_values[i], ebno[i])
+        plt.figure(2)
+        plt.plot(time_values[i], ber[i])
+
+plt.figure(1)
+plt.title("Downlink Eb/No")
+plt.xlabel("Time (EpSec)")
+plt.ylabel("Eb/No (dB)")
+
+plt.figure(2)
+plt.title("Downlink Bit Error Rate")
+plt.xlabel("Time (EpSec)")
+plt.ylabel("BER")
+
+plt.show()
 
 print("Saving...")
 root.Save()
